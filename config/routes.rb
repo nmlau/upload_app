d2bea4e6274e9863b34399c62f71819442b38137
Nicholas Lau <nicholas.m.lau@gmail.com>
2014-04-02 00:05:58 -0700

    added static page home and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 UploadApp::Application.routes.draw do
+  
+  root  'static_pages#home'
+  match 'home', to: 'static_pages#home',   via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
