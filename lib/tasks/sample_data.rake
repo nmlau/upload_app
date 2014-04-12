0a559f64771413c5e5f3fd76da8095bead3ccb65
Nicholas Lau <nicholas.m.lau@gmail.com>
2014-04-12 16:26:49 -0700

    got the devise capybara rspec login to work

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -2,7 +2,7 @@ namespace :db do
   desc "Fill database with sample data"
   task populate: :environment do
     make_users
-    make_uploads
+    #make_uploads
   end
 end
 
--- spec/requests/photo_pages_spec.rb
+++ spec/requests/photo_pages_spec.rb
@@ -14,3 +14,28 @@
 
 #to do the edit story
 #http://cloudinary.com/documentation/rails_carrierwave
+
+require 'spec_helper'
+include Warden::Test::Helpers
+Warden.test_mode!
+
+describe "Photo pages" do
+
+  subject { page }
+
+  let(:user) { FactoryGirl.create(:user) }
+
+  before do
+    login_as(user, :scope => :user)
+    visit root_path
+  end
+
+  it { should have_link('Sign out',    href: destroy_user_session_path) }
+  it { should have_link('Photos',       href: photos_path) }
+  it { should have_link('Settings',    href: edit_user_registration_path(user)) }
+  it { should_not have_link('Sign in',    href: new_user_session_path) }
+
+
+
+  Warden.test_reset! 
+end
